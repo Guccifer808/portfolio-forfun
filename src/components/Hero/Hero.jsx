73200650f2e29d9fc09fc2b76c18682db0c7aead
G808 <hucciallstars@gmail.com>
2023-01-19 22:17:27 +0200

    Hero edits

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,36 @@
 import css from './Hero.module.scss';
+import { motion } from 'framer-motion';
+import { fadeIn, staggerContainer } from './../../utils/motion';
 
 const Hero = () => {
   return (
-    <section className={css.wrapper}>
-      <div className={`innerWidth ${css.container}`}>
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
         {/* upper text */}
         <div className={css.upperElements}>
-          <span>
+          <motion.span
+            variants={fadeIn('right', 'tween', 0.2, 1)}
+            className='primaryText'
+          >
             I'm <br /> Portfolio
-          </span>
+          </motion.span>
           <span>
             I build a beautiful websites
             <br /> And I love what I do
           </span>
         </div>
+        {/* middle person div */}
+
+        <div className={css.person}>
+          <img src='./person.png' alt='person' />
+        </div>
+
         {/* lower text */}
         <div className={css.lowerElements}>
           <div className={css.experience}>
@@ -24,12 +41,12 @@ const Hero = () => {
             </div>
           </div>
           <div className={css.certificate}>
-            <img src='./certificate.png' alt='cetrificate image' />
+            <img src='./certificate.png' alt='cetrificate' />
             <span>CERTIFIED</span>
             <span>FRONT-END DEVELOPER</span>
           </div>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
